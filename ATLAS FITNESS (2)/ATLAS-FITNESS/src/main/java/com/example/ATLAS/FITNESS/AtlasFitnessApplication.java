cb55d75bb79b747034124a58aac5b383b24353ce
sol <sol.cl8750@gmail.com>
2025-12-08 22:16:35 -0500

    inicio

---
+++ ATLAS FITNESS (2)/ATLAS-FITNESS/src/main/java/com/example/ATLAS/FITNESS/AtlasFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.example.ATLAS.FITNESS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasFitnessApplication.class, args);
+	}
+
+}
---
+++ ATLAS FITNESS (2)/ATLAS-FITNESS/src/test/java/com/example/ATLAS/FITNESS/AtlasFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ATLAS.FITNESS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
